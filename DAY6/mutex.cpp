f2966f2675862e5e2f33a1af2f9a3ea157598b81
KiloMing <3402684841@qq.com>
2026-09-03 03:50:29 +0800

    code

--- DAY6/mutex.cpp
+++ DAY6/mutex.cpp
@@ -7,49 +7,31 @@
 #include <sys/wait.h>
 #include <pthread.h>
 
-struct SharedData
+pthread_mutex_t mutex;
+void* work(void *arg)
 {
-    int count;
-    //if you don't define this variable，the mutex will not be shared
-    pthread_mutex_t mutex;
-};
+    pthread_mutex_lock(&mutex);
+    std::cout << "work" << std::endl;
+    pthread_mutex_unlock(&mutex);
+
+    return nullptr; //don't forget the return
+}
 
 int main(void)
 {
+    //create the procecss
+    pthread_t t1;
+    pthread_t t2;
 
-    SharedData *shared = static_cast<SharedData *>(
-        mmap(
-            nullptr,
-            sizeof(SharedData),
-            PROT_READ | PROT_WRITE,
-            MAP_SHARED | MAP_ANONYMOUS, // MAP_ANONYMOUS : don't relay on the file
-            -1, // don't map file
-            0
-        )
-    );
-    shared->count = 0;
-    pid_t pid = fork();
-    if(pid < 0)
-    {
-        perror("fork");
-        return 1;
-    }
-    if(pid == 0)
-    {
-        //child process
-        pthread_mutex_lock(&shared->mutex);
-        shared->count++;
-        std::cout << shared->count << std::endl;
-        pthread_mutex_unlock(&shared->mutex);
-    }
-    else
-    {
-        // parent process
-        pthread_mutex_lock(&shared->mutex);
-        shared->count++;
-        std::cout << shared->count << std::endl;
-        pthread_mutex_unlock(&shared->mutex);
-        wait(nullptr);
-        pthread_mutex_destroy(&shared->mutex);
-    }
+    pthread_mutex_init(&mutex, nullptr);
+
+    pthread_create(&t1, nullptr, work, nullptr);
+    pthread_create(&t2, nullptr, work, nullptr);
+
+    pthread_join(t1, nullptr);
+    pthread_join(t2, nullptr);
+
+    std::cout << "*****end*****" << std::endl;
+
+    pthread_mutex_destroy(&mutex);
 }
